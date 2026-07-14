e73d042be82a008f11122e38cab319ad42377545
sergio penalosa <penalosa.sergiobono@gmail.com>
2026-07-14 23:50:47 +0800

    Added chart js logic to display a sample chart.

---
+++ script.js
@@ -0,0 +1,17 @@
+// Chart
+
+const breakdownChart = document.getElementById('breakdownChart');
+
+const bar = new Chart(breakdownChart, {
+    type: 'bar',
+    data: {
+        labels: ['Food', 'Transportation', 'Hobbies', 'Bills'],
+        datasets: [{
+            label: 'Expense Breakdown',
+            data: [4000, 2000, 1000, 12000]
+        }]
+    },
+    options: {},
+})
+
+
