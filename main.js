2103b899158f7ff5ce2b4175faeb9e28f9e030c1
sergio penalosa <penalosa.sergiobono@gmail.com>
2026-07-14 16:38:25 +0800

    Configured main.js with set values.

--- main.js
+++ main.js
@@ -1,13 +1,24 @@
+console.log('running...')
+
 const { app, BrowserWindow } = require('electron');
 
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 592,
-        height: 666
-    })
+        height: 666,
+        resizable: false,
+        maximizable: false,
+        frame: true,
+        fullscreenable: false,
+        transparent: false,
+        webPreferences: {
+            contextIsolation: true
+        }
+    });
+    win.setMenu(null);
     win.loadFile('index.html');
 }
 
 app.whenReady().then(() => {
-    createWindow()
+    createWindow()  
 })
