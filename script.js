b30d2c45343b0bc8eacc531d9ea17ac4a4793413
sergio penalosa <penalosa.sergiobono@gmail.com>
2026-07-16 11:03:05 +0800

    Added chart styling.

--- script.js
+++ script.js
@@ -2,14 +2,18 @@
 
 const breakdownChart = document.getElementById('breakdownChart');
 
+
 const bar = new Chart(breakdownChart, {
     type: 'bar',
     data: {
         labels: ['Food', 'Transportation', 'Hobbies', 'Bills'],
         datasets: [{
             label: 'Expense Breakdown',
-            data: [4000, 2000, 1000, 12000]
-        }]
+            data: [4000, 2000, 1000, 12000],
+            backgroundColor: '#523E67',
+            hoverBorderWidth: 4,
+            hoverBorderColor: '#000',
+        }],
     },
     options: {},
 })
